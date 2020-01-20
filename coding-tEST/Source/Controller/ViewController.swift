655556b119cafa3a82c6c44fb438cc84b8467af6
Henry Chukwu <henrychukwu13@gmail.comn>
2020-01-20 15:55:11 +0100

    hook up table view

--- coding-tEST/Source/Controller/ViewController.swift
+++ coding-tEST/Source/Controller/ViewController.swift
@@ -10,12 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var commits = [String]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
         MyGitHub.shared.getGithubUser()
     }
+}
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return commits.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "commitCell") as? CommitCell else { return UITableViewCell() }
+        
+        let commit = commits[indexPath.row]
+        
+        
+        return cell
+    }
+    
+    
 }
 
